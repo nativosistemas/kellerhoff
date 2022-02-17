c0a419bf254be1e8e51a7853b12e6c463529ab11
nativosistemas <nativosistemas@outlook.com.ar>
2022-02-17 01:31:30 -0300

    [DONE] isPedirCC

--- Kellerhoff/includes/mvc/js/buscadorCAR.js
+++ Kellerhoff/includes/mvc/js/buscadorCAR.js
@@ -1729,9 +1729,10 @@ function OnCallBackRecuperarProductos(args) {
                                 if (listaProductosBuscados[i].listaSucursalStocks[iSucursal].stk_codsuc === listaSucursal[iEncabezadoSucursal]) {
                                     var isMostrarImputSucursal = true;
                                     var isMostrarImputPerfu = true;
+                                    var is_pedirCC = true;
                                     strHtml += '<div class="' + getNameClassStock(listaProductosBuscados[i].listaSucursalStocks[iSucursal].stk_stock) + '"></div>';
                                     if (isMostrarImput) {
-                                        isMostrarImput = isMostrarImput_pedirCC(listaProductosBuscados[i].pro_codtpopro, listaSucursal[iEncabezadoSucursal], listaProductosBuscados[i].listaSucursalStocks);
+                                        is_pedirCC = isMostrarImput_pedirCC(listaProductosBuscados[i].pro_codtpopro, listaSucursal[iEncabezadoSucursal], listaProductosBuscados[i].listaSucursalStocks);
                                     }
                                     if (isMostrarImput) { //(pIndexSucursal, pIndexProducto)
                                         isMostrarImputSucursal = isMostrarImput_FacturaTrazablesProvincia(listaSucursal[iEncabezadoSucursal], listaProductosBuscados[i].pro_isTrazable);
@@ -1742,7 +1743,7 @@ function OnCallBackRecuperarProductos(args) {
                                     //if (listaSucursal[iEncabezadoSucursal] == "CB" && listaProductosBuscados[i].pro_codtpopro == "P") {
                                     //    isMostrarImputPerfu = false;
                                     //}
-                                    if (isMostrarImput && isMostrarImputSucursal && isMostrarImputPerfu) {
+                                    if (isMostrarImput && isMostrarImputSucursal && isMostrarImputPerfu && is_pedirCC) {
                                         // Cargar Cantidad
                                         var cantidadDeProductoEnCarrito = '';
                                         if (isSubirPedido) {
@@ -2894,8 +2895,9 @@ function detalleProducto_celular(pIndex) {
                 if (listaProductosBuscados[pIndex].listaSucursalStocks[iSucursal].stk_codsuc === listaSucursal[iEncabezadoSucursal]) {
                     var isMostrarImputSucursal = true;
                     var isMostrarImputPerfu = true;
+                    var is_pedirCC = true;
                     if (isMostrarImput) {
-                        isMostrarImput = isMostrarImput_pedirCC(listaProductosBuscados[pIndex].pro_codtpopro, listaSucursal[iEncabezadoSucursal], listaProductosBuscados[pIndex].listaSucursalStocks);
+                        is_pedirCC = isMostrarImput_pedirCC(listaProductosBuscados[pIndex].pro_codtpopro, listaSucursal[iEncabezadoSucursal], listaProductosBuscados[pIndex].listaSucursalStocks);
                     }
                     //if (listaSucursal[iEncabezadoSucursal] == "CB" && listaProductosBuscados[pIndex].pro_codtpopro == "P") {
                     //    isMostrarImputCB = false
@@ -2906,7 +2908,7 @@ function detalleProducto_celular(pIndex) {
                     if (isMostrarImput && listaProductosBuscados[pIndex].pro_codtpopro == "P") {
                         isMostrarImputPerfu = MostrarImputPerfu(listaProductosBuscados[pIndex].pro_codtpopro, listaSucursal[iEncabezadoSucursal], listaProductosBuscados[pIndex].listaSucursalStocks);
                     }
-                    if (isMostrarImput && isMostrarImputSucursal && isMostrarImputPerfu) {
+                    if (isMostrarImput && isMostrarImputSucursal && isMostrarImputPerfu && is_pedirCC) {
                         // Cargar Cantidad
                         var cantidadDeProductoEnCarrito = '';
                         if (isSubirPedido) {
